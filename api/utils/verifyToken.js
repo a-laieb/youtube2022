861bce3f7f5eb9b8d4fac573813b15cf484e3485
a-laieb <98498010+a-laieb@users.noreply.github.com>
2022-07-05 18:20:17 +0100

    Update verifyToken.js
    removed next in lines 17 and 27 because it will jump always in next of verifyToken() and skip[ the condition,
    also in verifyToken() i replaced "next" in the lines 7 and 10 with "res"
    the code is working in all cases now with logged and non logged cases

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -3,19 +3,18 @@ import { createError } from "../utils/error.js";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
-  if (!token) {
-    return next(createError(401, "You are not authenticated!"));
-  }
-
-  jwt.verify(token, process.env.JWT, (err, user) => {
-    if (err) return next(createError(403, "Token is not valid!"));
-    req.user = user;
-    next();
-  });
+    if(!token){
+        return res.status(401).json("You are not authenticated!");
+    }
+    jwt.verify(token, process.env.JWT, (err,user)=>{
+        if(err) return res.status(403).json("Token is not valid");
+        req.user = user;
+        next();
+    });
 };
 
 export const verifyUser = (req, res, next) => {
-  verifyToken(req, res, next, () => {
+  verifyToken(req, res, () => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next();
     } else {
@@ -25,7 +24,7 @@ export const verifyUser = (req, res, next) => {
 };
 
 export const verifyAdmin = (req, res, next) => {
-  verifyToken(req, res, next, () => {
+  verifyToken(req, res, () => {
     if (req.user.isAdmin) {
       next();
     } else {
